076d38712b606eaab756a9b12fca9eae212983a5
its-me-sujan <itsmesujangit@gmail.com>
2023-06-25 14:57:42 +0545

    written script for currency converter from url

---
+++ routes/routes.ui.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const CC = require("currency-converter-lt");
+
+router.get("/", (req, res) => {
+  res.send({ msg: "Hello from UI" });
+});
+
+router.get("/converter/:currency1/:currency2/:value", async (req, res) => {
+  const { currency1, currency2, value } = req.params; //params get value in string
+  const currencyConverter = new CC({
+    form: currency1,
+    to: currency2,
+    amount: Number(value),
+  });
+  const result = await currencyConverter.convert();
+  res.send(`Converted Amount : ${result}`);
+});
+
+module.exports = router;
