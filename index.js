716e19dbea1b991ae3f9df985b7cfa65a3d23f66
its-me-sujan <itsmesujangit@gmail.com>
2023-06-27 13:38:38 +0545

    injected middleware in indexjs

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 
 const app = express();
 
-const indexRouter = require('./routes');
+const indexRouter = require("./routes");
 
 // app.METHOD(PATH, HANDLER)
 app.use("/", indexRouter);
 
-app.listen(8090, () => {
-    console.log("error running on port 8000");
+app.listen(8000, () => {
+  console.log("sever running on port 8000");
 });
 
 // params '/:' diyera lekhyo bhane
