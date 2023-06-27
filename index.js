c21e7fb8461e645e4f24c84ac94f33e11d4dc8c1
its-me-sujan <itsmesujangit@gmail.com>
2023-06-27 14:06:18 +0545

    installed cors package

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const ejs = require("ejs");
 
 const app = express();
 
 const indexRouter = require("./routes");
 
-// middleware
-// header token, router, ip block
-app.use((req, res, next) => {
-  console.log("Request received at " + Date.now());
-  next();
-});
+// // middleware
+// // header token, router, ip block
+// app.use((req, res, next) => {
+//   console.log("Request received at " + Date.now());
+//   next();
+// });
 
 // app.METHOD(PATH, HANDLER)
+// setting up the third party middlewares
+app.use(morgan("short"));
+app.use(cors());
+
+// setting up the EJS Templating
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.use("/", indexRouter);
 
-app.listen(8000, () => {
+app.listen(8090, () => {
   console.log("sever running on port 8000");
 });
 
