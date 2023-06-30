884f7b5a4bb7b30a68a1d048b6b6ec83e2cb55a6
its-me-sujan <itsmesujangit@gmail.com>
2023-06-30 15:07:23 +0545

    added response for submitting the form

--- routes/routes.api.js
+++ routes/routes.api.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 //mostly error handler is done in routes api
 router.get("/", (req, res, next) => {
-    try {
-        res.json({msg:"Hello from API"});
-    } catch (err) {
-        next(err);
-    }
+  try {
+    res.json({ msg: "Hello from API" });
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.post("/form", (req, res, next) => {
-    console.log({ data: req.body });
-    res.json({ msg: "Hello form FORM api"});
+  console.log({ data: req.body });
+  res.json({ msg: "Thank you for submitting the form!" });
 });
-module.exports = router;   
+module.exports = router;
