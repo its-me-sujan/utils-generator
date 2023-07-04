85a2b97a827d2a9bb467886e36a2c5214254ad1e
its-me-sujan <itsmesujangit@gmail.com>
2023-07-04 14:30:45 +0545

    controller connected to corresponding routes

--- modules/contacts/contact.routes.api.js
+++ modules/contacts/contact.routes.api.js
@@ -1,32 +1,61 @@
+const contactController = require("./contact.controller");
 const express = require("express");
 const router = express.Router();
 
-const contactController = require("./contact.controller");
+/* Routes
+1. create (POST) => Payload
+2. list (GET)   => 
+3. getById (GET) => id params
+4. update (PUT) => id params, payload
+5. delete (DELETE) => id params
+*/
 
-router.get("/", (req, res, next) => {
-  res.json({ msg: "Hello from contact API!" });
-});
-
+// Import all the controller and connect it to the corresponding routes
 // Create
 router.post("/", (req, res, next) => {
-  contactController.create(req.body);
-  res.json({ msg: "Thank you for submitting the form!" });
+  contactController.create(req.body).then((d) => res.json(d)).catch((e) => next(e));
 });
 
 // List
-router.get("/list", async (req, res, next) => {
-  console.log({ data: req.body });
-  const list = await contactController.list();
-  res.json({ data: list });
+router.get("/", async (req, res, next) => {
+  try {
+    const list = await contactController.list();
+    res.json(list);
+  } catch(e) {
+    next(e);
+  }
 });
 
 // getById
-router.get("", async(req, res, next) => {});
+router.get("/:id", async(req, res, next) => {
+  try {
+    const result = await contactController.getById(req.params.id);
+    res.json(result);
+  } catch(e) {
+    next(e);
+  }
+});
 
 // update
-router.set("", async(req, res, next) => {});
+router.put("/:id", async(req, res, next) => {
+  try {
+    const id = ewq.params.id;
+    const payload = req.body;
+    const result = await contactController.update(id, payload);
+  } catch(e) {
+    next(e);
+  }
+});
 
 // remove
-router.delete("", async(req, res, next) => {});
+router.delete("/:id", async(req, res, next) => {
+  try {
+    const id = req.params.id;
+    const list = await contactController.remove(id);
+    res.json(result);
+  } catch(e) {
+    next(e);
+  }
+});
 
 module.exports = router;
