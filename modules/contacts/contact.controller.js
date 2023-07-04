b027d1b51a2952d5ad14b399913ce8530f3a4578
its-me-sujan <itsmesujangit@gmail.com>
2023-07-04 14:17:52 +0545

    added crud operations

--- modules/contacts/contact.controller.js
+++ modules/contacts/contact.controller.js
@@ -1,25 +1,24 @@
-const Model = require("./contact.model");
+const contactModel = require("./contact.contact.model");
 
+// CRUD Ops
 const create = (payload) => {
-    // writing in DB
-    return Model.create(payload);
+    return contactModel.create(payload);
 };
 
 const list = () => {
-    // pagination
-    return Model.find();
+    return contactModel.find();
 };
 
 const getById = (id) => {
-    return Model.findOne(id);
+    return contactModel.findOne({_id: id});
 };
 
 const update = (id, payload) => {
-    return Model.updateOne(id, payload);
+    return contactModel.updateOne({_id: id}, payload);
 };
 
 const remove = (id) => {
-    return Model.deleteOne(id);
+    return contactModel.deleteOne({_id: id});
 };
 
 module.exports = {create, list, getById, update, remove};
